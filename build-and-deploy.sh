b5c4c46943b2386714f8011be14d19538aada8ee
Joonwoo Park <93jpark@gmail.com>
2026-04-20 11:31:19 +0900

    fix typo

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 sudo docker build -t aipub-brewery-web:0.0.1 .
-sudo docker save -o web.tar aipub-brewery-web:0.0.1 .
+sudo docker save -o web.tar aipub-brewery-web:0.0.1
 sudo ctr -n k8s.io images import web.tar
 sudo kubectl rollout restart deploy -n aipub aipub-brewery-web
 rm -f web.tar
